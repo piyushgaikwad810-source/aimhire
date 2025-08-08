eba5e59e4a4ae18fb09f59304b02fee9bbf261e2
piyushgaikwad810-source <piyushgaikwad810@gmail.com>
2025-08-08 14:04:06 +0530

    Add files via upload

---
+++ problem_5.py
@@ -0,0 +1,19 @@
+a = 5
+b = 6
+print(a==b)
+# result is false
+
+a = 8
+b = 8
+print(a==b)
+# result is true
+
+a = 8
+b = 8
+print(a!=b)
+# result is false  
+
+a = 8
+b = 6
+print(a!=b)
+# result is true
